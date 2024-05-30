4248b2fad74f0dc62b50fb4d4b0f07efc22bcb50
Nikita Kolmogorov <backmeupplz@gmail.com>
2024-05-30 12:13:42 -0700

    remove comment

--- src/helpers/getUsername.ts
+++ src/helpers/getUsername.ts
@@ -1,9 +1,6 @@
 import axios from 'axios'
 
 export default async function (address: string) {
-  // fetch https://farcaster-data.vercel.app/api/wallet-info?address=${address}
-  // return username
-
   const {
     data: { data },
   } = await axios.get(
