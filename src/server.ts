9d040ab4b781e557e7488545d9bfda4a78bf7a27
Nikita Kolmogorov <backmeupplz@gmail.com>
2024-05-30 11:22:31 -0700

    remove boilerplate

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,6 @@
 import 'module-alias/register'
 import 'source-map-support/register'
 
-import runApp from '@/helpers/runApp'
-import runMongo from '@/helpers/mongo'
-
 void (async () => {
-  console.log('Starting mongo')
-  await runMongo()
-  console.log('Mongo connected')
-  await runApp()
+  console.log('nice')
 })()
